7cf2a9b8c38e5b98dadc7af85e4b7285883847d2
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-09-05 14:45:33 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatesAndTimesNotes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
